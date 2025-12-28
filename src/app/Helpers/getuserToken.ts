38aa859622dc785e6d649ed443b9abd132ec76b5
LidiaNabil <lidianabil98@gmail.com>
2025-12-28 07:44:15 +0200

    token update

--- src/app/Helpers/getuserToken.ts
+++ src/app/Helpers/getuserToken.ts
@@ -2,7 +2,8 @@ import { cookies } from 'next/headers'
 import { decode } from 'next-auth/jwt'
 
 export async function getUserToken() {
-        const x = (await cookies()).get('next-auth.session-token')?.value
+    const myCookies = await cookies()
+    const x = myCookies.get('next-auth.session-token')?.value  ||   myCookies.get('__secure-next-auth.session-token')?.value
     const accessToken = await decode({ token: x, secret: process.env.AUTH_SECRET! })
     return accessToken?.token
 }
